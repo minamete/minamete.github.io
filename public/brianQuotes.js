03b9e05228dceed0140fc90476b446e948c6d041
minamete <348563206@gapps.yrdsb.ca>
2021-03-05 00:48:58 -0500

    no more messing with async js's added more content

--- public/brianQuotes.js
+++ public/brianQuotes.js
@@ -1,27 +1,34 @@
 
 
 const brianData = [
-"im about to fuck the shit out of everyone",
-"im feeling homosexual today should i go hit up aram",
-"im sure Aram will learn to accept my true feelings when the time is right",
-"so now Zekiel has more sexual knowledge than me. unacceptable. i must attend church services as well",
-"i'm a minority, I can't be racist",
-"My penis, when fully erect, is actually longer than I am tall.",
-"i thought I'd never say this but I really want to see you guys again",
-"ima have to spank andy later",
-"i can't believe I'm so bad at attracting and mating",
-"even lichess knows it's an embarrassment to lose to me",
-"hey. it's been a week bitch",
-"why would i be taking notes when I can be an anime protagonist in another world",
-"I've always wanted to meet a hentai artist",
-"the semi slav? is that when you only chug half a bottle of vodka?",
-"i've been too busy feeling up my history essays",
-"I've been lowering my standards for the last 16 years. oh wait i'm 17",
-"let's say for the sake of argument that you weren't a broke ass bitch",
-"wow that sounded more brain dead than your vegetable patients",
-"im practicing chess puzzles because i want to beat up laura",
-"have you had experience with male horniness",
-"LSD? nah. heroine maybe"
-]
-
-export default brianData;
+    "im about to fuck the shit out of everyone",
+    "im feeling homosexual today should i go hit up aram",
+    "im sure Aram will learn to accept my true feelings when the time is right",
+    "so now Zekiel has more sexual knowledge than me. unacceptable. i must attend church services as well",
+    "i'm a minority, I can't be racist",
+    "My penis, when fully erect, is actually longer than I am tall.",
+    "i thought I'd never say this but I really want to see you guys again",
+    "ima have to spank andy later",
+    "i can't believe I'm so bad at attracting and mating",
+    "even lichess knows it's an embarrassment to lose to me",
+    "hey. it's been a week bitch",
+    "why would i be taking notes when I can be an anime protagonist in another world",
+    "I've always wanted to meet a hentai artist",
+    "the semi slav? is that when you only chug half a bottle of vodka?",
+    "i've been too busy feeling up my history essays",
+    "I've been lowering my standards for the last 16 years. oh wait i'm 17",
+    "let's say for the sake of argument that you weren't a broke ass bitch",
+    "wow that sounded more brain dead than your vegetable patients",
+    "im practicing chess puzzles because i want to beat up laura",
+    "have you had experience with male horniness",
+    "LSD? nah. heroine maybe",
+    "what's cp, contribution points?",
+    "When I feel pain, I feel successful, then I feel happy.",
+    "i'm not into incest, but-",
+    "well, no, incest is fine",
+    "You would have to strangle me and call me Onii-chan for me to write another English essay",
+    "Homosexual people are not known for their excellent memory",
+    "okay i'd like to remind everyone; I don't sound like a loser for obsessing over a virtual girl's thighs, I sound like a loser because I am one"
+    ]
+    
+    export default brianData;
