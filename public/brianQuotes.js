1fd5554b1a82868e44e2b07434686da94637600c
minamete <348563206@gapps.yrdsb.ca>
2021-03-04 22:06:43 -0500

    even more variety, blackmailed from brian himself

--- public/brianQuotes.js
+++ public/brianQuotes.js
@@ -4,8 +4,8 @@ const brianData = [
 "im about to fuck the shit out of everyone",
 "im feeling homosexual today should i go hit up aram",
 "im sure Aram will learn to accept my true feelings when the time is right",
-"I enjoy stimulating my urethra with large objects",
-"My very first sexual experience was with a goat. No partner could satisfy me since",
+"so now Zekiel has more sexual knowledge than me. unacceptable. i must attend church services as well",
+"i'm a minority, I can't be racist",
 "My penis, when fully erect, is actually longer than I am tall.",
 "i thought I'd never say this but I really want to see you guys again",
 "ima have to spank andy later",
@@ -18,7 +18,10 @@ const brianData = [
 "i've been too busy feeling up my history essays",
 "I've been lowering my standards for the last 16 years. oh wait i'm 17",
 "let's say for the sake of argument that you weren't a broke ass bitch",
-"wow that sounded more brain dead than your vegetable patients"
+"wow that sounded more brain dead than your vegetable patients",
+"im practicing chess puzzles because i want to beat up laura",
+"have you had experience with male horniness",
+"LSD? nah. heroine maybe"
 ]
 
 export default brianData;
