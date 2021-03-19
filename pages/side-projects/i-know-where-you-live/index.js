0d3b60d9ed0cf77afe7c8c50fa5f8c651c674744
minamete <348563206@gapps.yrdsb.ca>
2021-03-19 17:59:30 -0400

    got rid of my api key, now i can make this public again

--- pages/side-projects/i-know-where-you-live/index.js
+++ pages/side-projects/i-know-where-you-live/index.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import {StyledOhio} from "../../../styles/Ohio.styled"
 
-const API_KEY = "AIzaSyB1OfPBvSQOx3QNmfVvplDjH9HPXM04UF0";
-const URL = "https://maps.google.com/maps/api/geocode/json?latlng=";
 const OHIO = [40.4173, -82.9071];
 
 export default function WhereAmI() {
@@ -21,24 +19,13 @@ export default function WhereAmI() {
         function setPosition({ coords: { latitude, longitude } }) {
             setLongitude(longitude);
             setLatitude(latitude);
-            let posToJson = fetch(`${URL}${latitude},${longitude}&key=${API_KEY}`).then((resp) => {
-                return resp.json();
-            })
-            let fetchResults = posToJson.then(({ results: [{ formatted_address }] }) => {
-                setAddress(formatted_address);
-            });
-            let fetchWarning = fetchResults.then(() => {
-                let ohioDistance = distance(OHIO[0], OHIO[1], latitude, longitude);
-                setDistanceFromOhio(ohioDistance);
-                
-            })
+            
+            let ohioDistance = distance(OHIO[0], OHIO[1], latitude, longitude);
+            setDistanceFromOhio(ohioDistance);
+
         }
         navigator.geolocation.getCurrentPosition(setPosition);
-        let watcher = navigator.geolocation.watchPosition(
-            setPosition,
-            err => console.error(err),
-            { enableHighAccuracy: true }
-        );
+
         return () => {
             navigator.geolocation.clearWatch(watcher);
         };
@@ -47,7 +34,6 @@ export default function WhereAmI() {
     return (
         <StyledOhio>
 
-            <h1>Current Address (estimated): {address}</h1>
             <h1>Latitude: {latitude.toFixed(4) + ' °N'}</h1>
             <h1>Longitude: {longitude.toFixed(4) + ' °W'}</h1>
             <h1>Distance from Ohio: {distanceFromOhio.toFixed(4)} km</h1>
