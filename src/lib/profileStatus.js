d32af252653fb0711afb9dc9fea3e6d11043b265
Laura Cao <yzcao@uwaterloo.ca>
2026-07-07 21:13:44 -0400

    update unemployment snippet to reflect my visa issues :(

--- src/lib/profileStatus.js
+++ src/lib/profileStatus.js
@@ -16,11 +16,11 @@ export function getProfileStatus(today = new Date()) {
   const location = today.getMonth() >= 7 ? "San Francisco" : "Waterloo";
 
   const occupation =
-    today.getMonth() >= 7
+    today.getMonth() >= 8
       ? "I work as a software engineer at Meta"
       : today.getMonth() >= 5
         ? "I'm currently an unemployed bum after my exams"
-        : "I'm a soon-to-be graduate from the University of Waterloo, where I studied computer science and math";
+        : "I'm a soon-to-be graduate from the University of Waterloo, where I studied computer science and math. I am currently an unemployed bum";
 
   return { location, occupation };
 }
@@ -28,8 +28,9 @@ export function getProfileStatus(today = new Date()) {
 export const locationSnippet = `  const location = today.getMonth() >= 7 ? "San Francisco" : "Waterloo"`;
 
 export const occupationSnippet = `  const occupation =
-    today.getMonth() >= 7
+    today.getMonth() >= 8
       ? "I work as a software engineer at Meta"
       : today.getMonth() >= 5
         ? "I'm currently an unemployed bum after my exams"
-        : "I'm a soon-to-be graduate from the University of Waterloo, where I studied computer science and math"`;
+        : "I'm a soon-to-be graduate from the University of Waterloo, where I studied computer science and math. I am currently an unemployed bum";
+`;
